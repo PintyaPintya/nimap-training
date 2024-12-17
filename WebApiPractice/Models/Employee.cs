7453c93aa3590475851865e9fd245003e5fe3f6b
PintyaPintya <omkajale09@gmail.com>
2024-12-17 09:49:03 +0530

    Dto

---
+++ WebApiPractice/Models/Employee.cs
@@ -0,0 +1,11 @@
+namespace WebApiPractice.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+    public int Salary { get; set; }
+    public string Department { get; set; }
+}
---
+++ WebApiPractice/Models/EmployeeDto.cs
@@ -0,0 +1,9 @@
+namespace WebApiPractice.Models;
+
+public class EmployeeDto
+{
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+    public string Department { get; set; }
+}
